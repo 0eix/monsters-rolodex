af958ba728213c72304986288668a993023ebd99
Elisee Othniel AMEGASSI <eliseothniel@gmail.com>
2019-09-13 15:42:29 +0000

    Updates

---
+++ precache-manifest.25f4a15999c1fdbfd9e3119f9ce48b3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8102fc02dc42ab069c54a3c03da6786d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c6ee017596bd3d080c9c",
+    "url": "/monsters-rolodex/static/css/main.65655fbb.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "c6ee017596bd3d080c9c",
+    "url": "/monsters-rolodex/static/js/main.8e0cdc84.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
